69af6fc4a0a209768123e493896d43251e8af97c
Laure <laure.milian@gmail.com>
2016-11-14 10:20:42 +0100

    post sent

--- appexpress.js
+++ appexpress.js
@@ -1,11 +1,18 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.post('/checkUser', function(req, res) {
+	console.log(req.body.inputPassword);
+	res.redirect('/access.html');
+})
 
 
 app.listen(2000);
--- public/app.js
+++ public/app.js
@@ -2,8 +2,34 @@
 
 var app = {
 
+	endpoint: 'http://localhost:2000',
+
 	init: function() {
-		$('h1').css('color', 'blue');
+		app.listeners();
+	},
+
+	listeners: function() {
+		$('#submitBtn').on('click', this.checkUser.bind(this));
+	},
+
+	checkUser: function() {
+		var inputLogin = $('#inputLogin').val();
+		var inputPassword = $('#inputPassword').val();
+		$.post({
+			url: this.endpoint + '/checkUser',
+			method: 'POST',
+			data: {inputLogin: inputLogin, inputPassword: inputPassword}
+		})
+		.done(this.checkUserDone)
+		.fail(this.checkUserFail);
+	},
+
+	checkUserDone: function() {
+		console.log('ok');
+	},
+
+	checkUserFail: function() {
+		console.log('nope');
 	}
 }
 
